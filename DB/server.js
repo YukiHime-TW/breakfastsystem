b41a8487be0c2b5121a090dce9e3be7edaffd769
PaLlEn0801 <74391659+PaLlEn0801@users.noreply.github.com>
2020-12-11 15:20:20 +0800

    Add files via upload

--- DB/server.js
+++ DB/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-
-const SingleRoutes = require('./routes/singleroute')
-mongoose.connect('mongodb://localhost:27017/breakfast', 
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
+var SingleRoutes = require('./routes/singleroute')
+var insert  = require('./SingleStore.js')
+var single = require('./model/single')
+mongoose.connect('mongodb://localhost:27017/Breakfast', 
 {useNewUrlParser: true, 
 useCreateIndex: true, 
 useUnifiedTopology: true})
@@ -23,7 +25,14 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
+var name, price, description;
+app.post('/insert', urlencodedParser, function (req, res) {
+    name = req.body.Name;
+    price = req.body.Price;
+    description = req.body.Introduce;
+    insert.insertone(name, price, description);
+    res.sendFile(__dirname + '/finish.html');
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
