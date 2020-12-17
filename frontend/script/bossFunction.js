0aa87d6664b54cf9a998c92c4d50fc3d398567a5
YukiHime <majiku6@gmail.com>
2020-12-17 11:46:06 +0800

    Update bossFunction.js

--- frontend/script/bossFunction.js
+++ frontend/script/bossFunction.js
@@ -51,35 +51,3 @@ function init() {
     };
     request.send(null);
 }
-
-function addmenu() {
-    var json = JSON.parse(request.response);
-    let d = document.getElementById("main");
-    let d_nest = document.getElementById("menu");
-    d.removeChild(d_nest);
-
-    var menu = document.createElement("div");
-    menu.id = "menu";
-
-    for (var i = 0; i < json.length; i++) {
-        var newDiv = document.createElement("div");
-        newDiv.style =
-            "width: 25%; border-width:3px;border-style:solid;border-color:black;padding:5px; margin-left: 15%; margin-top: 20%;";
-        newDiv.id = i;
-        newDiv.textContent = json[i].proName + json[i].proPrice;
-        menu.appendChild(newDiv);
-    }
-    var plusMenu = document.createElement("div");
-    var plusImg = document.createElement("img");
-    plusImg.src = "../image/plus.png";
-    plusImg.style = "width: 100%";
-    plusMenu.appendChild(plusImg);
-    plusMenu.style =
-        "width: 25%; border-width:3px;border-style:solid;border-color:black;padding:5px; margin-left: 15%; margin-top: 20%;";
-    plusMenu.onclick = function () {
-        window.open("editmenuplus.html", "1234");
-    };
-    plusMenu.id = "regis";
-    menu.appendChild(plusMenu);
-    d.appendChild(menu);
-}
