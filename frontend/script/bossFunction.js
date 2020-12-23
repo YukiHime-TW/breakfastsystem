2a2c9ae42565b726144cc5cf9b477b3bcfd190e9
YukiHime-TW <majiku6@gmail.com>
2020-12-23 17:20:34 +0800

    Update bossFunction.js

--- frontend/script/bossFunction.js
+++ frontend/script/bossFunction.js
@@ -49,3 +49,34 @@ function init() {
     };
     request.send(null);
 }
+
+function editInit() {
+    request.open("GET", url, true);
+    request.onload = function () {
+        var json = JSON.parse(request.response);
+        document.getElementById("food_id").value = json[0]._id;
+        document.getElementById("name").value = json[0].food_name;
+        document.getElementById("description").value = json[0].description;
+        document.getElementById("price").value = json[0].price;
+        console.log(json);
+    };
+    request.send(null);
+}
+
+function btnOperate(op)
+ {
+    var value = Number(document.getElementById("num").value);
+    if (op == '+') {
+        value += 1;
+    } else if (op == '-') {
+        if (value <= 1) {
+            value = 1;
+        } else {
+            value -= 1;
+        }
+    }
+    var json = JSON.parse(request.response);
+    document.getElementById("num").value = value;
+    var price = document.getElementById("price").value;
+    document.getElementById("price").value = json[0].price * document.getElementById("num").value;
+}
