d3e469505ab685350f564d89b88b493c77867399
YukiHime-TW <majiku6@gmail.com>
2020-12-23 17:20:36 +0800

    Update customerFunction.js

--- frontend/script/customerFunction.js
+++ frontend/script/customerFunction.js
@@ -1,5 +1,6 @@
 var request = new XMLHttpRequest();
-var url = "https://raw.githubusercontent.com/YukiHime-TW/breakfastsystem/master/frontend/script/test.json";
+var url =
+  "https://raw.githubusercontent.com/YukiHime-TW/breakfastsystem/master/frontend/script/test.json";
 var div = new Array(0);
 var image = new Array(0);
 
@@ -34,13 +35,45 @@ function init() {
   request.send(null);
 }
 
+function menuInit() {
+  request.open("GET", url, true);
+  request.onload = function () {
+      var json = JSON.parse(request.response);
+      document.getElementById("food_id").value = json[0]._id;
+      document.getElementById("name").value = json[0].food_name;
+      document.getElementById("description").value = json[0].description;
+      document.getElementById("price").value = json[0].price;
+      console.log(json);
+  };
+  request.send(null);
+}
+
+function btnOperate(op)
+ {
+    var value = Number(document.getElementById("num").value);
+    if (op == '+') {
+        value += 1;
+    } else if (op == '-') {
+        if (value <= 1) {
+            value = 1;
+        } else {
+            value -= 1;
+        }
+    }
+    var json = JSON.parse(request.response);
+    document.getElementById("num").value = value;
+    var price = document.getElementById("price").value;
+    document.getElementById("price").value = json[0].price * document.getElementById("num").value;
+}
+
 function addDish(i) {
   var json = JSON.parse(request.response);
-  localStorage.setItem('cartKey' + i, div[i].id);
-  div[i].num++;
-  localStorage.setItem('cartKey' + i + "num", div[i].num);
   var cart = document.getElementById("cart");
-  var putIn = document.createElement("input");
+  var alreadyDish = document.getElementByValue(`div[${i}].num`);
+  if (alreadyDish) {
+  } else {
+    var putIn = document.createElement("input");
+  }
   putIn.type = "hidden";
   putIn.value = div[i].id;
   putIn.id = json[i].food_name;
@@ -48,10 +81,10 @@ function addDish(i) {
   cart.appendChild(putIn);
 }
 
-function minusDish(i){
+function minusDish(i) {
   var json = JSON.parse(request.response);
   div[i].num--;
-  localStorage.setItem('cartKey' + i + "num", div[i].num);
+  localStorage.setItem("cartKey" + i + "num", div[i].num);
   var cart = document.getElementById("cart");
   cart.removeChild(document.getElementById(json[i].food_name));
 }
