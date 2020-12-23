15bea84a8bd15abc0a20e01a0386eff5804eab76
YukiHime <majiku6@gmail.com>
2020-12-24 01:01:42 +0800

    Update customerFunction.js

--- frontend/script/customerFunction.js
+++ frontend/script/customerFunction.js
@@ -38,47 +38,45 @@ function init() {
 function menuInit() {
   request.open("GET", url, true);
   request.onload = function () {
-      var json = JSON.parse(request.response);
-      document.getElementById("food_id").value = json[0]._id;
-      document.getElementById("name").value = json[0].food_name;
-      document.getElementById("description").value = json[0].description;
-      document.getElementById("price").value = json[0].price;
-      console.log(json);
+    var json = JSON.parse(request.response);
+    document.getElementById("food_id").value = json[0]._id;
+    document.getElementById("name").value = json[0].food_name;
+    document.getElementById("description").value = json[0].description;
+    document.getElementById("price").value = json[0].price;
+    console.log(json);
   };
   request.send(null);
 }
 
-function btnOperate(op)
- {
-    var value = Number(document.getElementById("num").value);
-    if (op == '+') {
-        value += 1;
-    } else if (op == '-') {
-        if (value <= 1) {
-            value = 1;
-        } else {
-            value -= 1;
-        }
+function btnOperate(op) {
+  var value = Number(document.getElementById("num").value);
+  if (op == '+') {
+    value += 1;
+  } else if (op == '-') {
+    if (value <= 1) {
+      value = 1;
+    } else {
+      value -= 1;
     }
-    var json = JSON.parse(request.response);
-    document.getElementById("num").value = value;
-    var price = document.getElementById("price").value;
-    document.getElementById("price").value = json[0].price * document.getElementById("num").value;
+  }
+  var json = JSON.parse(request.response);
+  document.getElementById("num").value = value;
+  var price = document.getElementById("price").value;
+  document.getElementById("price").value = json[0].price * document.getElementById("num").value;
 }
 
 function addDish(i) {
   var json = JSON.parse(request.response);
   var cart = document.getElementById("cart");
-  var alreadyDish = document.getElementByValue(`div[${i}].num`);
-  if (alreadyDish) {
+  var alreadyDish = document.getElementById(`div[${i}].food_name`);
+  if (alreadyDish == null) {
   } else {
-    var putIn = document.createElement("input");
+    var putIn = document.createElement("input"); putIn.type = "hidden";
+    putIn.value = div[i].id;
+    putIn.id = json[i].food_name;
+    putIn.name = "Cart";
+    cart.appendChild(putIn);
   }
-  putIn.type = "hidden";
-  putIn.value = div[i].id;
-  putIn.id = json[i].food_name;
-  putIn.name = "Cart";
-  cart.appendChild(putIn);
 }
 
 function minusDish(i) {
