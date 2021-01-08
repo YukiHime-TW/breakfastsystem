35132f70a2ce9759f25c32f1143581e563d16af1
Steve Shen <golddragon11@gmail.com>
2021-01-08 15:37:35 +0800

    modify order model

--- DB/model/order.js
+++ DB/model/order.js
@@ -2,21 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const orderSchema = new Schema({
+    order_num: Number,
     user_id: {
         type: String
     },
-    single:[{
-        food_id: String, // singleObjectID
-        finished: Boolean
+    food_id:[{
+        "id": String,
+        "name": String, 
+        "amount": Number
     }],
-    set_: [{
-        set_id: String, // setObjectID
-        finished: Boolean
+    set_id: [{
+        "id":String,
+        "amount": Number    
     }],
-    state:{
-        type: Number  // 送出:1 製作中:2 待領取:3 已領取:4
+    state:[{
+        type: Number  // 送出:1 製作中:2 待領取(已完成) :3 已領取: 4
+    }],
+    price:{
+        type: Number
     }
-},{timestamps: true})
+},{timestamps: true}) // default timestamp format : CreatedAt UpdatedAt
 
 var order = mongoose.model('order',orderSchema)
 module.exports = order;
